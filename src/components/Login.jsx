56d693990c79ae79015890f0b1b2da4d00580d07
PamelaCanaza <lunapamela886@gmail.com>
2025-09-13 22:01:01 -0400

    feat: agregar función handleSubmit para Login

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -2,7 +2,16 @@ import React, { useState } from "react";
 
 const Login = () => {
     const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");  };
+    const [password, setPassword] = useState(""); 
+    const handleSubmit = (e) => {
+        e.preventDefault();
+         if (email && password) {
+        onLogin({ email });
+        } else {
+            alert("Ingrese correo y contraseña");
+        }
+    };
+ };
 <div style={{ maxWidth: "400px", margin: "auto", paddingTop: "50px" }}>
   <h2>Iniciar Sesión</h2>
   <form>
