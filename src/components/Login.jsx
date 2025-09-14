b297151b858f1ac7ad83f3cd913c6f4ac58bfddc
PamelaCanaza <lunapamela886@gmail.com>
2025-09-13 21:39:19 -0400

    feat: agregar estructura basica del formulario en Login

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,6 +1,19 @@
 import React from "react";
 
-const Login = () => <div></div>;
+const Login = () => <div>
+  <h2>Iniciar Sesión</h2>
+  <form>
+    <div>
+      <label>Email:</label>
+      <input type="email" />
+    </div>
+    <div>
+      <label>Contraseña:</label>
+      <input type="password" />
+    </div>
+    <button>Ingresar</button>
+  </form>
+</div>;
 
 export default Login;
 
