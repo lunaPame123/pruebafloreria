ce638955def36e96d39631c45e5984b17e191e51
PamelaCanaza <lunapamela886@gmail.com>
2025-09-13 21:53:49 -0400

    feat: agregar estados useState para email y contraseña en Login

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,6 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 
-const Login = () => <div style={{ maxWidth: "400px", margin: "auto", paddingTop: "50px" }}>
+const Login = () => {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");  };
+<div style={{ maxWidth: "400px", margin: "auto", paddingTop: "50px" }}>
   <h2>Iniciar Sesión</h2>
   <form>
     <div>
